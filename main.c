40ec5335426326074b870359608d4da2a501c1ec
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-06-06 23:37:23 +0530

    justnowcreated

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+  input_buffer->buffer=NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+  
+  return input_buffer;
+}
+void print_prompt() { printf("db > "); }
+
+int main(int argc, char* argv[]) {
+  InputBuffer* input_buffer = new_input_buffer();
+  while(true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      close_input_buffer(input_buffer);
+      exit(EXIT_SUCCESS);
+    } else{
+      printf("Unrecognized command '%s' .\n");
+    }
+  }
+}
