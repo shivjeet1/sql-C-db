bde514087988f3b0456b8eab3ec2b23dffd1b11b
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-06-08 18:36:39 +0530

    pt2-p2-done

--- main.c
+++ main.c
@@ -9,6 +9,13 @@ typedef enum{
 } MetaCommandResult;
 
 typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT} PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
+
 typedef struct {
   char* buffer;
   size_t buffer_length;
@@ -41,6 +48,40 @@ void close_input_buffer(InputBuffer* input_buffer) {
 }
 
 void print_prompt() { printf("db > "); }
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+  if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if(strcmp(input_buffer->buffer, "select") == 0){
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("Beginning Insertion.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("Selection start.\n");
+      break;
+  }
+}
+
+
 
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
