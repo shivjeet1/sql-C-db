6b3c50df3d2243a005f88e1e60a614ffad7c78c3
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-06-08 08:02:55 +0530

    pt1-done

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 typedef struct {
   char* buffer;
@@ -15,6 +17,23 @@ InputBuffer* new_input_buffer() {
   
   return input_buffer;
 }
+
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+  if(bytes_read<=0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+  //Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
+
 void print_prompt() { printf("db > "); }
 
 int main(int argc, char* argv[]) {
@@ -25,9 +44,10 @@ int main(int argc, char* argv[]) {
 
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
       close_input_buffer(input_buffer);
+      printf("Exit successfull");
       exit(EXIT_SUCCESS);
     } else{
-      printf("Unrecognized command '%s' .\n");
+      printf("Unrecognized command '%s' .\n", input_buffer->buffer);
     }
   }
 }
