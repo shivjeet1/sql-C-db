7df3d6f102407814503db357a422e593bf92a6c2
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-06-08 18:12:39 +0530

    pt2-p1

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+typedef enum{
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT} PrepareResult;
 typedef struct {
   char* buffer;
   size_t buffer_length;
@@ -42,12 +48,26 @@ int main(int argc, char* argv[]) {
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      printf("Exit successfull");
-      exit(EXIT_SUCCESS);
-    } else{
-      printf("Unrecognized command '%s' .\n", input_buffer->buffer);
+    if(input_buffer->buffer[0] == '.'){
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case(META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
+    
+    Statement statement;
+    switch(prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+        continue;
+    }
+    
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
