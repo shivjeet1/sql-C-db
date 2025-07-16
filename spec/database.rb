a46a719780da5066fbed0dfbe34a8500de0768b6
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-07-16 21:57:05 +0530

    somethings work

--- spec/database.rb
+++ spec/database.rb
@@ -8,7 +8,6 @@ describe 'database' do
 
       pipe.close_write
 
-
       # Read entire output
       raw_output = pipe.gets(nil)
     end
@@ -28,62 +27,5 @@ describe 'database' do
       "db > ",
     ])
   end
-
-  it 'prints error message when table is full' do
-    script = (1..1401).map do |i|
-      "insert #{i} user#{i} person#{i}@example.com"
-    end
-    script << ".exit"
-    result = run_script(script)
-    expect(result[-2]).to eq('db > Error: Table full.')
-  end
-
-  it 'allows inserting strings that are the maximum length' do
-    long_username = "a"*32
-    long_email = "a"*255
-    script = [
-      "insert 1 #{long_username} #{long_email}",
-      "select",
-      ".exit",
-    ]
-    result = run_script(script)
-    expect(result).to match_array([
-      "db > Executed.",
-      "db > (1, #{long_username}, #{long_email})",
-      "Executed.",
-      "db > ",
-    ])
-  end
-
-  it 'prints error message if strings are too long' do
-    long_username = "a"*33
-    long_email = "a"*256
-    script = [
-      "insert 1 #{long_username} #{long_email}",
-      "select",
-      ".exit",
-    ]
-    result = run_script(script)
-    expect(result).to match_array([
-      "db > String is too long.",
-      "db > Executed.",
-      "db > ",
-    ])
-  end
-
-=begin
-  it 'prints an error message if id is negative' do
-    script = [
-      "insert -1 cstack foo@bar.com",
-      "select",
-      ".exit",
-    ]
-    result = run_script(script)
-    expect(result).to match_array([
-      "db > ID must be positive.",
-      "db > Executed.",
-      "db > ",
-    ])
-  end
-=end
 end
+
