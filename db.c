2dd41271ed1a1df4e583dcce54f52584358c86ce
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-07-28 23:39:51 +0530

    maybe-maybenot

--- db.c
+++ db.c
@@ -281,8 +281,14 @@ Table* db_open(const char* filename) {
 
 	Table* table = malloc(sizeof(Table));
 	table->pager = pager;
-	table->num_rows = num_rows;
-	
+	table->root_page_num = 0;
+
+	if (pager->num_pages == 0) {
+	//New database file. Initialize page 0 as leaf node.
+	void* root_node = get_page(pager, 0);
+	initialize_leaf_node(root_node);
+	}
+
 	return table;
 }
 
