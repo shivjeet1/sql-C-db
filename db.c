004e564c4880e082a3879e27158c86fb85f2a8f3
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-08-03 17:21:04 +0530

    added internal node

--- db.c
+++ db.c
@@ -6,12 +6,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
+
 #define SIZE 255
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-#define TABLE_MAX_PAGES 100
 
 typedef struct {
 	char* buffer;
@@ -19,11 +15,54 @@ typedef struct {
 	ssize_t input_length;
 } InputBuffer;
 
+typedef enum { 
+	EXECUTE_SUCCESS,
+	EXECUTE_DUPLICATE_KEY,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
+
+typedef enum{
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { 
+	PREPARE_SUCCESS,
+	PREPARE_SYNTAX_ERROR,
+	PREPARE_NEGATIVE_ID,
+	PREPARE_STRING_TOO_LONG,
+	PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
 typedef struct {
 	uint32_t id;
-  char email[COLUMN_EMAIL_SIZE + 1];
   char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
+
+typedef struct {
+	StatementType type;
+	Row row_to_insert; //only used by insert statement
+} Statement;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+#define INVALID_PAGE_NUM UINT32_MAX
 
 typedef struct {
 	int file_descriptor;
@@ -44,44 +83,13 @@ typedef struct {
 	bool end_of_table;
 } Cursor;
 
-enum ExecuteResult_t { 
-	EXECUTE_SUCCESS,
-	EXECUTE_DUPLICATE_KEY,
-       	EXECUTE_TABLE_FULL };
-
-typedef enum{
-	META_COMMAND_SUCCESS,
-	META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
-
-typedef enum { 
-	PREPARE_SUCCESS,
-	PREPARE_SYNTAX_ERROR,
-	PREPARE_NEGATIVE_ID,
-	PREPARE_STRING_TOO_LONG,
-	PREPARE_UNRECOGNIZED_STATEMENT
-} PrepareResult;
+void print_row(Row* row) {
+	printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
 
 typedef enum {
 	NODE_INTERNAL, NODE_LEAF
 } NodeType;
-
-typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
-
-typedef struct {
-	StatementType type;
-	Row row_to_insert; //only used by insert statement
-} Statement;
-
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
-const uint32_t PAGE_SIZE = 4096;
 
 /*Common Node Header Layout*/
 const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
@@ -93,6 +101,23 @@ const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
 const uint8_t COMMON_NODE_HEADER_SIZE =
 	NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
 
+/*Internal Node Header Layout*/
+ const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
+ const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET = COMMON_NODE_HEADER_SIZE;
+ const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
+ const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET =
+	 INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE;
+ const uint32_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE +
+											INTERNAL_NODE_NUM_KEYS_SIZE +
+											INTERNAL_NODE_RIGHT_CHILD_SIZE;
+/*Internal Node Body Layout*/
+ const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
+ const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
+ const uint32_t INTERNAL_NODE_CELL_SIZE =
+	 INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
+ /* Keep this small for testing */
+ const uint32_t INTERNAL_NODE_MAX_KEYS = 3;
+ 
 /*Leaf Node Header Layout*/
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
@@ -175,10 +200,6 @@ void close_input_buffer(InputBuffer* input_buffer) {
 }
 
 void print_prompt() { printf("db > "); }
-
-void print_row(Row* row) {
-	printf("(%d, %s, %s)\n", row->id, row->username, row->email);
-}
 
 void serialize_row(Row* source, void* destination) {
 	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
